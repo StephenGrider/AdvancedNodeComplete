c5b907f1a38e5f662b4c65d0cd5ff554cb12936f
stephengrider <ste.grider@gmail.com>
2018-02-19 16:10:58 -0800

    hydrate

--- AdvancedNodeStarter/routes/blogRoutes.js
+++ AdvancedNodeStarter/routes/blogRoutes.js
@@ -14,32 +14,9 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
-    const redisUrl = 'redis://127.0.0.1:6379';
-    const client = redis.createClient(redisUrl);
-    const util = require('util');
-    client.get = util.promisify(client.get);
-
-    // Do we have any cached data in redis related
-    // to this query
-    const cachedBlogs = await client.get(req.user.id);
-
-    // if yes, then respond to the request right away
-    // and return
-    if (cachedBlogs) {
-      console.log('SERVING FROM CACHE');
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    // if no, we need to respond to request
-    // and update our cache to store the data
-
     const blogs = await Blog.find({ _user: req.user.id });
 
-    console.log('SERVING FROM MONGODB');
     res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
