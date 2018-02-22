eaa1cad2ef6c788c8eea95043f75015ddc332dc0
stephengrider <ste.grider@gmail.com>
2018-02-21 16:30:44 -0800

    factory

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -31,6 +31,21 @@ test('clicking login starts oauth flow', async () => {
 test('When signed in, shows logout button', async () => {
   const id = '5a85dc351cd843163727f63c';
 
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
   await page.setCookie({ name: 'session', value: sessionString });
   await page.setCookie({ name: 'session.sig', value: sig });
   await page.goto('localhost:3000');
