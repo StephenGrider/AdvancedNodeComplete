47e0cb3366c65381cfe9f8b7b3bc02277563a3c1
stephengrider <ste.grider@gmail.com>
2018-02-22 16:36:27 -0800

    lookup

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -1,19 +1,16 @@
-const puppeteer = require('puppeteer');
 const sessionFactory = require('./factories/sessionFactory');
 const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-  browser = await puppeteer.launch({
-    headless: false
-  });
-  page = await browser.newPage();
+  page = await Page.build();
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-  await browser.close();
+  await page.close();
 });
 
 test('the header has the correct text', async () => {
---
+++ AdvancedNodeStarter/tests/helpers/page.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+class CustomPage {
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+
+    return new Proxy(customPage, {
+      get: function(target, property) {
+        return customPage[property] || browser[property] || page[property];
+      }
+    });
+  }
+
+  constructor(page) {
+    this.page = page;
+  }
+}
+
+module.exports = CustomPage;
