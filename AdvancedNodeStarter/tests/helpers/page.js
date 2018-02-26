f070395fea6c5bb911dcad1332162c4e1844a57a
stephengrider <ste.grider@gmail.com>
2018-02-26 15:36:24 -0800

    test helper

--- AdvancedNodeStarter/tests/helpers/page.js
+++ AdvancedNodeStarter/tests/helpers/page.js
@@ -35,6 +35,43 @@ class CustomPage {
   async getContentsOf(selector) {
     return this.page.$eval(selector, el => el.innerHTML);
   }
+
+  get(path) {
+    return this.page.evaluate(_path => {
+      return fetch(_path, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json());
+    }, path);
+  }
+
+  post(path, data) {
+    return this.page.evaluate(
+      (_path, _data) => {
+        return fetch(_path, {
+          method: 'POST',
+          credentials: 'same-origin',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(_data)
+        }).then(res => res.json());
+      },
+      path,
+      data
+    );
+  }
+
+  execRequests(actions) {
+    return Promise.all(
+      actions.map(({ method, path, data }) => {
+        return this[method](path, data);
+      })
+    );
+  }
 }
 
 module.exports = CustomPage;
