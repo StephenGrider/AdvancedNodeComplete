e5ae9bb6d2406e717464251a248f2878c950966c
stephengrider <ste.grider@gmail.com>
2018-03-15 09:35:07 -0700

    boilerplate

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -7,29 +7,16 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = async function() {
+mongoose.Query.prototype.exec = function() {
+  console.log('IM ABOUT TO RUN A QUERY');
+
   const key = JSON.stringify(
     Object.assign({}, this.getQuery(), {
       collection: this.mongooseCollection.name
     })
   );
 
-  // See if we have a value for 'key' in redis
-  const cacheValue = await client.get(key);
+  console.log(key);
 
-  // If we do, return that
-  if (cacheValue) {
-    const doc = JSON.parse(cacheValue);
-
-    return Array.isArray(doc)
-      ? doc.map(d => new this.model(d))
-      : new this.model(doc);
-  }
-
-  // Otherwise, issue the query and store the result in redis
-  const result = await exec.apply(this, arguments);
-
-  client.set(key, JSON.stringify(result));
-
-  return result;
+  return exec.apply(this, arguments);
 };
